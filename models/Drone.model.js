275b1daaa9bc3698948cb93f0854fc290458c743
Ioannis Tourtouras <yiannistourtce@gmail.com>
2022-03-16 21:12:33 +0200

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
 // Iteration #1
+const {model, Schema} = require("mongoose")
+// the two vars are available from mongoose
+const droneSchema = new Schema({
+    name: {type: String, required: true},
+    propellers: {type: Number, required: true},
+    maxSpeed: {type: Number, required: true},
+})
+
+const droneModel = model('drone', droneSchema)
+module.exports = droneModel;
